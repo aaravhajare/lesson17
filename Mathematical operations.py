97276795d01a2c798540d181246e0d9f8f7b5688
aaravhajare <hajareaarav66@gmail.com>
2025-03-31 12:13:29 +0530

    Mathematical operations

---
+++ Mathematical operations.py
@@ -0,0 +1,14 @@
+import math
+
+print(math.ceil(5.4))
+
+print(math.floor(6.6))
+
+x = 10
+y = -15
+
+print(math.copysign(x,y))
+
+print((math.fabs(-2334), (math.fabs(234))))
+
+print(math.gcd(5,10))
